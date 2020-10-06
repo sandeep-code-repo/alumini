c1923e816fb82e987bcc3cae76348659f6793697
Ksamal366 <k.ksamal366@gmail.com>
2020-10-06 11:32:45 +0530

    Excel to DB Component added

--- src/main/java/com/skeleton/alumini/constants/ApplicationConstants.java
+++ src/main/java/com/skeleton/alumini/constants/ApplicationConstants.java
@@ -3,5 +3,6 @@ package com.skeleton.alumini.constants;
 public class ApplicationConstants {
 	
 	public static final String API_OVER_ALL_SUCCESS_STATUS = "success"; 
+	public static final String API_OVER_ALL_FAILED_STATUS = "Failed"; 
 
 }
