63c4357dd3179c823391d06a61e1960b4eda1dfd
Ksamal366 <k.ksamal366@gmail.com>
2020-10-08 20:07:14 +0530

    Application prop for server and Security java file build error fixed

--- src/main/java/com/skeleton/alumini/util/SecurityKeyUtil.java
+++ src/main/java/com/skeleton/alumini/util/SecurityKeyUtil.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.skeleton.alumini.util;
 
 
@@ -7,58 +6,6 @@ import java.security.Key;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import sun.misc.*;
-
-public class SecurityKeyUtil {
-	
-	private static final String ALGORITHM = "AES";
-    private static final byte[] keyValue = 
-    		new byte[] { 'J', 'A', 'Y', 'A', 'S', 'H', 'R', 'E', 'E', 'c', 'r', 'e', 't', 'K', 'e', 'y' };
-    // Consider in mind before changing the Key-Value as it's used 17 bit length 
-    // To Generate Key for encryption as well as decryption
-    
-    
-    @SuppressWarnings("restriction")
-    public static String encrypt(String valueToEnc) throws Exception {
-        Key key = generateKey();
-        Cipher c = Cipher.getInstance(ALGORITHM);
-        c.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encValue = c.doFinal(valueToEnc.getBytes());        
-        return new BASE64Encoder().encode(encValue);
-    }
- 
-    
-    @SuppressWarnings("restriction")
-    public static String decrypt(String encryptedValue) throws Exception {
-        Key key = generateKey();
-        Cipher c = Cipher.getInstance(ALGORITHM);
-        c.init(Cipher.DECRYPT_MODE, key);
-        byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedValue);
-		byte[] decValue = c.doFinal(decordedValue);
-		return new String(decValue);
-    }
- 
-    private static Key generateKey() throws Exception {
-        return new SecretKeySpec(keyValue, ALGORITHM);
-    }
-    
-    
-    public static void main(String args[]) throws Exception {
-    	String encryptPassword = encrypt("jayashree#");
-		System.out.println("encryptPassword : " +  encryptPassword);
-		
-		String decryptPassword = decrypt(encryptPassword);
-		System.out.println("decryptPassword : " +  decryptPassword);
-		
-		if (decryptPassword.equals("jayashree#1")) {
-			System.out.println("matched");
-		} else {
-			System.out.println("invalid");
-		}
-	}
-
-}
-=======
-package com.skeleton.alumini.util;
 import java.security.Key;
 import java.util.Base64;
 
@@ -113,4 +60,4 @@ public class SecurityKeyUtil {
 
 
 }
->>>>>>> ad226becfad2cb45b50b16f75e6ce15858386517
+
